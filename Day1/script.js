802d7d9336bc55cd39c52d9dec0f6ce5c71cc0be
Abdallah Elwasiy <abdallahelwasify0gmail.com>
2026-07-12 21:08:07 +0300

    Add Day1 JavaScript Task

---
+++ Day1/script.js
@@ -0,0 +1,15 @@
+var num1 = Number(window.prompt("Enter First Number"));
+var num2 = Number(window.prompt("Enter Second Number"));
+
+console.log("Add: " + (num1 + num2));
+console.log("Sub: " + (num1 - num2));
+console.log("Mul: " + (num1 * num2));
+console.log("Div: " + (num1 / num2));
+console.log("Mod: " + (num1 % num2));
+
+var firstName = window.prompt("Enter First Name");
+var lastName = window.prompt("Enter Last Name");
+var age = Number(window.prompt("Enter Your Age"));
+
+console.log("Welcome " + firstName + " " + lastName);
+console.log("Age: " + age);
